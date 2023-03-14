8759193cad799fdcfd7f5d660dc2950174abcb1a
Jawairia Malik <jawairiamalik268@gmail.com>
2023-03-14 14:05:45 -0400

    PIN 10 LED fading - stable

--- Fade/Fade.ino
+++ Fade/Fade.ino
@@ -1,16 +1,19 @@
 
-//Prgramer : JoJo Malik
-//date: 3.14.23
-//program: fade
+//Programer : JoJo Malik
+//Date: 3.14.23
+//Program: fade
 
 
 
+//Programer : JoJo Malik
+//Date: 3.14.23
+//Program: fade
 
 
 /*
   Fade
 
-  This example shows how to fade an LED on pin 9 using the analogWrite()
+  This example shows how to fade an LED on pin 10 using the analogWrite()
   function.
 
   The analogWrite() function uses PWM, so if you want to change the pin you're
@@ -22,19 +25,19 @@
   https://www.arduino.cc/en/Tutorial/BuiltInExamples/Fade
 */
 
-int led = 9;           // the PWM pin the LED is attached to
+int led = 10;           // the PWM pin the LED is attached to
 int brightness = 0;    // how bright the LED is
 int fadeAmount = 5;    // how many points to fade the LED by
 
 // the setup routine runs once when you press reset:
 void setup() {
-  // declare pin 9 to be an output:
+  // declare pin 10 to be an output:
   pinMode(led, OUTPUT);
 }
 
 // the loop routine runs over and over again forever:
 void loop() {
-  // set the brightness of pin 9:
+  // set the brightness of pin 10:
   analogWrite(led, brightness);
 
   // change the brightness for next time through the loop:
