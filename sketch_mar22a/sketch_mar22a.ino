683961ed0efcc8fda9854304d4ccf2940d5bcc42
Jawairia Malik <jawairiamalik268@gmail.com>
2023-03-22 13:59:10 -0400

    updated variables

---
+++ sketch_mar22a/sketch_mar22a.ino
@@ -0,0 +1,80 @@
+//Programmer : JoJo Malik
+//Program : Blind Spot Monitor
+
+
+int trigPin = 7;
+int echoPin = 6;
+int LEDlampRed = 9;
+int  LEDlampWhite = 10;
+int LEDlampBlue = 11;
+int soundbuzzer = 3;
+int sound  = 500;
+
+
+void setup() {
+  Serial.begin (9600);
+  pinMode(trigPin,  OUTPUT);
+  pinMode(echoPin, INPUT);
+  pinMode(LEDlampRed, OUTPUT);
+  pinMode(LEDlampYellow,  OUTPUT);
+  pinMode(LEDlampGreen, OUTPUT);
+  pinMode(soundbuzzer, OUTPUT);
+}
+void  loop() {
+  long durationindigit, distanceincm;
+  digitalWrite(trigPin, LOW);  
+  delayMicroseconds(2);
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigPin, LOW);
+  durationindigit = pulseIn(echoPin, HIGH);
+  distanceincm = (durationindigit/5) / 29.1;
+ 
+  if (distanceincm < 50) {
+      digitalWrite(LEDlampGreen, HIGH);
+}
+  else {
+      digitalWrite(LEDlampGreen,  LOW);
+  }
+  
+  if (distance < 20) {
+    digitalWrite(LEDlampYellow,  HIGH);
+}
+  else {
+    digitalWrite(LEDlampYellow,LOW);
+  }
+  if (distance  < 5) {
+    digitalWrite(LEDlampRed, HIGH);
+    sound = 1000;
+}
+  else  {
+    digitalWrite(LEDlampRed,LOW);
+  }
+ 
+  if (distanceincm > 5 ||  distanceinsm <= 0){
+    Serial.println("Outside the permissible range of distances");
+    noTone(soundbuzzer);
+  }
+  else {
+    Serial.print(distance);
+    Serial.println("  cm");
+    tone(buzzer, sound);
+  }
+  
+  delay(300);
+}
+
+
+
+
+
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
