2b52d00c327363840f0eb907ac4f5e1da37479a8
Jawairia Malik <jawairiamalik268@gmail.com>
2023-04-07 14:07:49 -0400

    changing to countdown clock

--- clock/clock.ino
+++ clock/clock.ino
@@ -1,66 +1,220 @@
-const int zero[7] = {HIGH, HIGH, HIGH, HIGH, HIGH, HIGH, LOW};
-const int one[7] = {LOW, HIGH, HIGH, LOW, LOW, LOW, LOW};
-const int two[7] = {HIGH, HIGH, LOW, HIGH, HIGH, LOW, HIGH};
-const int three[7] = {HIGH, HIGH, HIGH, HIGH, LOW, LOW, HIGH};
-const int four[7] = {LOW, HIGH, HIGH, LOW, LOW, HIGH, HIGH};
-const int five[7] = {HIGH, LOW, HIGH, HIGH, LOW, HIGH, HIGH};
-const int six[7] = {HIGH, LOW, HIGH, HIGH, HIGH, HIGH, HIGH};
-const int seven[7] = {HIGH, HIGH, HIGH, LOW, LOW, LOW, LOW};
-const int eight[7] = {HIGH, HIGH, HIGH, HIGH, HIGH, HIGH, HIGH};
-const int nine[7] = {HIGH, HIGH, HIGH, LOW, LOW, HIGH, HIGH};
-
-const int* digits[10] = {zero, one, two, three, four, five, six, seven, eight, nine};
-
-const int firstSegmentPin = 2;
-const int lastSegmentPin = 8;
-
-const int firstMuxPin = 9;
-const int lastMuxPin = 12;
-
-const int displayDigitCount = 4;
-
-void resetDisplay()
-{
-for (int i = firstMuxPin; i <= lastMuxPin; ++i)
-{
-digitalWrite(i, HIGH);
-}
-}
-
-void writeDisplayDigit(int value, int index)
-{
-for (int i = 0; i < 7; ++i)
-{
-digitalWrite(i + firstSegmentPin, digits[value][i]);
-}
-
-digitalWrite(index + firstMuxPin, LOW);
-}
-
-void writeDisplay(int values[displayDigitCount])
-{
-for (int i = 0; i < displayDigitCount; ++i)
-{
-resetDisplay();
-writeDisplayDigit(values[i], i);
-delay(1);
-}
-}
+int pinA = 2;
+int pinB = 3;
+int pinC = 4;
+int pinD = 5;
+int pinE = 6;
+int pinF = 7;
+int pinG = 8;
+int D1 = 9;
+int D2 = 10;
+int D3 = 11;
+int D4 = 12;
 
 // the setup routine runs once when you press reset:
-void setup()
-{
-for (int i = firstSegmentPin; i <= lastMuxPin; ++i)
-{
-pinMode(i, OUTPUT); // initialize the digital pins as outputs.
+void setup() {
+// initialize the digital pins as outputs.
+pinMode(pinA, OUTPUT);
+pinMode(pinB, OUTPUT);
+pinMode(pinC, OUTPUT);
+pinMode(pinD, OUTPUT);
+pinMode(pinE, OUTPUT);
+pinMode(pinF, OUTPUT);
+pinMode(pinG, OUTPUT);
+pinMode(D1, OUTPUT);
+pinMode(D2, OUTPUT);
+pinMode(D3, OUTPUT);
+pinMode(D4, OUTPUT);
 }
 
-resetDisplay();
+//NUMBERS:
+
+//0
+void n0(){
+digitalWrite(pinA, HIGH);
+digitalWrite(pinB, HIGH);
+digitalWrite(pinC, HIGH);
+digitalWrite(pinD, HIGH);
+digitalWrite(pinE, HIGH);
+digitalWrite(pinF, HIGH);
+digitalWrite(pinG, LOW);
+delay(1000); // wait for a second
 }
+
+//1
+void n1(){
+digitalWrite(pinA, LOW);
+digitalWrite(pinB, HIGH);
+digitalWrite(pinC, HIGH);
+digitalWrite(pinD, LOW);
+digitalWrite(pinE, LOW);
+digitalWrite(pinF, LOW);
+digitalWrite(pinG, LOW);
+delay(1000); // wait for a second
+}
+//2
+void n2(){
+digitalWrite(pinA, HIGH);
+digitalWrite(pinB, HIGH);
+digitalWrite(pinC, LOW);
+digitalWrite(pinD, HIGH);
+digitalWrite(pinE, HIGH);
+digitalWrite(pinF, LOW);
+digitalWrite(pinG, HIGH);
+delay(1000); // wait for a second
+}
+//3
+void n3(){
+digitalWrite(pinA, HIGH);
+digitalWrite(pinB, HIGH);
+digitalWrite(pinC, HIGH);
+digitalWrite(pinD, HIGH);
+digitalWrite(pinE, LOW);
+digitalWrite(pinF, LOW);
+digitalWrite(pinG, HIGH);
+delay(1000); // wait for a second
+}
+//4
+void n4(){
+digitalWrite(pinA, LOW);
+digitalWrite(pinB, HIGH);
+digitalWrite(pinC, HIGH);
+digitalWrite(pinD, LOW);
+digitalWrite(pinE, LOW);
+digitalWrite(pinF, HIGH);
+digitalWrite(pinG, HIGH);
+delay(1000); // wait for a second
+}
+//5
+void n5(){
+digitalWrite(pinA, HIGH);
+digitalWrite(pinB, LOW);
+digitalWrite(pinC, HIGH);
+digitalWrite(pinD, HIGH);
+digitalWrite(pinE, LOW);
+digitalWrite(pinF, HIGH);
+digitalWrite(pinG, HIGH);
+delay(1000); // wait for a second
+}
+//6
+void n6(){
+digitalWrite(pinA, HIGH);
+digitalWrite(pinB, LOW);
+digitalWrite(pinC, HIGH);
+digitalWrite(pinD, HIGH);
+digitalWrite(pinE, HIGH);
+digitalWrite(pinF, HIGH);
+digitalWrite(pinG, HIGH);
+delay(1000); // wait for a second
+}
+//7
+void n7(){
+digitalWrite(pinA, HIGH);
+digitalWrite(pinB, HIGH);
+digitalWrite(pinC, HIGH);
+digitalWrite(pinD, LOW);
+digitalWrite(pinE, LOW);
+digitalWrite(pinF, LOW);
+digitalWrite(pinG, LOW);
+delay(1000); // wait for a second
+}
+//8
+void n8(){
+digitalWrite(pinA, HIGH);
+digitalWrite(pinB, HIGH);
+digitalWrite(pinC, HIGH);
+digitalWrite(pinD, HIGH);
+digitalWrite(pinE, HIGH);
+digitalWrite(pinF, HIGH);
+digitalWrite(pinG, HIGH);
+delay(1000); // wait for a second
+}
+//9
+void n9(){
+digitalWrite(pinA, HIGH);
+digitalWrite(pinB, HIGH);
+digitalWrite(pinC, HIGH);
+digitalWrite(pinD, HIGH);
+digitalWrite(pinE, LOW);
+digitalWrite(pinF, HIGH);
+digitalWrite(pinG, HIGH);
+delay(1000); // wait for a second
+}
+//0 blinking
+void n00(){
+digitalWrite(pinA, HIGH);
+digitalWrite(pinB, HIGH);
+digitalWrite(pinC, HIGH);
+digitalWrite(pinD, HIGH);
+digitalWrite(pinE, HIGH);
+digitalWrite(pinF, HIGH);
+digitalWrite(pinG, LOW);
+delay(1000); // wait for a second
+digitalWrite(pinA, HIGH);
+digitalWrite(pinB, HIGH);
+digitalWrite(pinC, HIGH);
+digitalWrite(pinD, HIGH);
+digitalWrite(pinE, HIGH);
+digitalWrite(pinF, HIGH);
+digitalWrite(pinG, LOW);
+delay(100); // wait for a second
+}
+
+void s1(){
+digitalWrite(D1, LOW);
+digitalWrite(D2, HIGH);
+digitalWrite(D3, HIGH);
+digitalWrite(D4, HIGH);
+}
+
+void s2(){
+digitalWrite(D1, HIGH);
+digitalWrite(D2, LOW);
+digitalWrite(D3, HIGH);
+digitalWrite(D4, HIGH);
+}
+
+void s3(){
+digitalWrite(D1, HIGH);
+digitalWrite(D2, HIGH);
+digitalWrite(D3, LOW);
+digitalWrite(D4, HIGH);
+}
+
+void s4(){
+digitalWrite(D1, HIGH);
+digitalWrite(D2, HIGH);
+digitalWrite(D3, HIGH);
+digitalWrite(D4, LOW);
+}
+
+
+
+
 
 // the loop routine runs over and over again forever:
-void loop()
-{
-int values[4] = {0,2,1,3};
-writeDisplay(values);
+void loop() {
+s4();
+n9();
+s3();
+n8();
+s2();
+n7();
+s1();
+n6();
+s2();
+n5();
+s3();
+n4();
+s4();
+n3();
+s3();
+n2();
+s2();
+n1();
+s1();
+s1();
+n0();
+s1();
+n00();
+
 }
